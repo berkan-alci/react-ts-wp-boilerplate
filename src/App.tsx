3cbba381e173cc1b85facc2390109d7ba144c23b
Berkan Alci <alci.berkan@gmail.com>
2022-05-21 14:02:19 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './styles.css';
 const App = () => {
     const name = "test";
     return (
-        <div>React Typescript starter template - {process.env.NODE_ENV}</div>
+        <div>React Typescript starter template - {process.env.NODE_ENV} - { name }</div>
     );
 };
 
